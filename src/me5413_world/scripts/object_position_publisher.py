f0b0a7e6b531d895e52d7ad1d3e0fb098a1e3061
Zhou Zihan <zihanzhou@u.nus.edu>
2024-04-11 22:46:07 +0800

    modify some parameters

--- src/me5413_world/scripts/object_position_publisher.py
+++ src/me5413_world/scripts/object_position_publisher.py
@@ -16,7 +16,7 @@ class ObjectPositionPublisher:
         self.objection_position_received = False
 
         # -------------------
-        self.match_counter = 0  # 连续匹配计数器
+        self.match_counter = 0  # Counter for consecutive matches
         # -------------------
 
         self.rate = rospy.Rate(10.0)
@@ -29,10 +29,10 @@ class ObjectPositionPublisher:
         #     self.send_goal(pose)
         if not self.objection_position_received:
             self.objection_position_received = True
-            self.match_counter += 1  # 收到匹配，计数器加1
+            self.match_counter += 1  # Match received, increment the counter
         else:
-            self.match_counter = 0  # 没有匹配，重置计数器
-        if self.match_counter >= 2:  # 连续匹配达到3帧
+            self.match_counter = 0  # No match, reset the counter
+        if self.match_counter >= 5:  # Consecutive matches reached 6 frames
             self.send_goal(pose)
 
     def send_goal(self, pose):
@@ -40,26 +40,26 @@ class ObjectPositionPublisher:
         processed_pose = self.process_goal(pose)
 
         try:
-            # 发布处理后的目标位置消息到 /move_base_simple/goal 话题上
+            # Publish the processed goal position message to the '/move_base_simple/goal' topic
             self.pub.publish(processed_pose)
             rospy.loginfo("Sending goal to move_base: {}".format(processed_pose))
         except rospy.ROSException as e:
             rospy.logerr("Failed to publish goal: {}".format(str(e)))
         
         try:
-            # 发布原始的目标位置消息到 /objection_position_pose 话题上
+            # Publish the original goal position message to the '/objection_position_pose' topic
             self.objection_position_pub.publish(pose)
         except rospy.ROSException as e:
             rospy.logerr("Failed to publish original goal: {}".format(str(e)))
     
     def process_goal(self, pose):
         try:
-            # 获取机器人当前位置和朝向
+            # Get the current position and orientation of the jackal
             (trans, rot) = self.listener.lookupTransform('/map', '/base_link', rospy.Time(0))
             x, y, z = trans[0], trans[1], trans[2]
             _, _, current_yaw = euler_from_quaternion(rot)
 
-            # 获取原始目标点位置
+            # Get the original target position
             original_x = pose.pose.position.x
             original_y = pose.pose.position.y
             _, _, original_yaw = euler_from_quaternion([
@@ -69,22 +69,22 @@ class ObjectPositionPublisher:
             pose.pose.orientation.w
             ])
 
-            # 计算目标位置的欧拉角，并调整为与当前方向最接近的正负90度朝向
+            # Calculate the Euler angles of the target position, and adjust to the nearest ±90 degrees direction to the current orientation
             target_yaw = current_yaw + math.copysign(math.pi / 2.0, current_yaw)
 
             target_x = original_x + 1 * math.cos(original_yaw)
             target_y = original_y + 1 * math.sin(original_yaw)
 
-            # 构造调整后的目标位置的四元数表示
+            # Construct the quaternion representation of the adjusted target position
             quat = quaternion_from_euler(0, 0, target_yaw)
 
-            # 创建处理后的目标点消息
+            # Create the processed goal point message
             processed_pose = PoseStamped()
             processed_pose.header.stamp = rospy.Time.now()
-            processed_pose.header.frame_id = pose.header.frame_id  # 与原始目标位置相同的坐标系
+            processed_pose.header.frame_id = pose.header.frame_id  # Same coordinate system as the original target position
             processed_pose.pose.position.x = target_x
             processed_pose.pose.position.y = target_y
-            processed_pose.pose.position.z = 0  # z 坐标设为 0
+            processed_pose.pose.position.z = 0  # Set z coordinate to 0
             processed_pose.pose.orientation = Quaternion(*quat)
 
             return processed_pose
